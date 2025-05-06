48114abd74e882588e1dbc63d4bd12a478b1bc9a
Elite_tea <andreewkirill64@yandex.ru>
2025-05-06 11:32:38 +0400

    del: Удалены неиспользуемые маперы doc: Добавлено документирование в спецификации JavaDoc refactor: Улучшена читаемость кода

--- src/main/java/ru/hometask/mappers/ClientMapper.java
+++ src/main/java/ru/hometask/mappers/ClientMapper.java
@@ -6,11 +6,25 @@ import ru.hometask.dto.NewClientDto;
 import ru.hometask.dto.OldClientDto;
 import ru.hometask.entities.Client;
 
+/**
+ * Маппер для преобразования между сущностью Client и DTO.
+ */
 @Mapper(componentModel = "spring")
 public interface ClientMapper {
 
+    /**
+     * Преобразует NewClientDto в сущность Client.
+     * Игнорирует поле id, так как оно генерируется автоматически.
+     * @param newClient DTO для создания нового клиента
+     * @return сущность Client
+     */
     @Mapping(target = "id", ignore = true)
     Client newUserMapping(NewClientDto newClient);
 
+    /**
+     * Преобразует сущность Client в OldClientDto.
+     * @param client сущность клиента
+     * @return DTO с данными клиента
+     */
     OldClientDto oldUserMapping(Client client);
 }
--- src/main/java/ru/hometask/mappers/ContractMapper.java
+++ src/main/java/ru/hometask/mappers/ContractMapper.java
@@ -9,9 +9,17 @@ import ru.hometask.entities.Contract;
 
 import java.util.List;
 
+/**
+ * Маппер для преобразования между сущностью Contract и DTO.
+ */
 @Mapper(componentModel = "spring")
 public interface ContractMapper {
 
+    /**
+     * Преобразует NewContractDto в сущность Contract.
+     * @param newContract DTO для создания нового договора
+     * @return сущность Contract
+     */
     @Mapping(target = "id", ignore = true)
     @Mapping(source = "clientId", target = "client.id")
     @Mapping(source = "employeeId", target = "employee.id")
@@ -19,19 +27,32 @@ public interface ContractMapper {
     @Mapping(source = "statusId", target = "status.id")
     Contract newContractMapping(NewContractDto newContract);
 
+    /**
+     * Преобразует сущность Contract в UpdateContractDto.
+     * @param contract сущность договора
+     * @return DTO для обновления договора
+     */
     @Mapping(source = "client.id", target = "clientId")
     @Mapping(source = "employee.id", target = "employeeId")
     @Mapping(source = "issuePoint.id", target = "issuePointId")
     @Mapping(source = "status.id", target = "statusId")
     UpdateContractDto oldContractMapping(Contract contract);
 
+    /**
+     * Преобразует OldContractDto в UpdateContractDto.
+     * @param contract DTO с данными договора
+     * @return DTO для обновления договора
+     */
     @Mapping(source = "client.id", target = "clientId")
     @Mapping(source = "employee.id", target = "employeeId")
     @Mapping(source = "issuePoint.id", target = "issuePointId")
     @Mapping(source = "status.id", target = "statusId")
     UpdateContractDto oldContractToUpdateMapping(OldContractDto contract);
 
-    @Mapping(source = "employee", target = "employee")
+    /**
+     * Преобразует список сущностей Contract в список OldContractDto.
+     * @param contract список сущностей договоров
+     * @return список DTO с данными договоров
+     */
     List<OldContractDto> noPasswordContractDTO(List<Contract> contract);
-
 }
